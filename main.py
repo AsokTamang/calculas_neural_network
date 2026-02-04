9c383976831fc98b39939a67927065bae8d2dbf9
AsokTamang <ashoktmg2058@gmail.com>
2026-02-04 14:43:02 -0500

    reading and plotting the data

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
 
+df = pd.read_csv("C:/pythonprojects/calculas_neural_network/datas/tvmarketing.csv")
+print(df)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
